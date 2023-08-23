76e439664adfff2bac5b27fcf377268c08dae863
collinwillis <collin.willis@outlook.com>
2023-08-23 12:14:44 -0700

    Initial Commit

---
+++ SwingMinder_POC/SwingMinder_POCApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwingMinder_POCApp.swift
+//  SwingMinder_POC
+//
+//  Created by Collin Willis on 8/23/23.
+//
+
+import SwiftUI
+
+@main
+struct SwingMinder_POCApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
